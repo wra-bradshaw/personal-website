4936ab19a9f62b575caaa2becfffefd813f70b49
will <will.bradshaw50@gmail.com>
2026-06-27 00:30:33 +1000

    feat: adjust padding mobile

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ export function Container({ className, ...props }: ComponentProps<"article">) {
   return (
     <article
       className={cn(
-        "max-w-lg mx-auto md:p-8 px-6 pt-3 md:mt-12 text-dark dark:text-light",
+        "max-w-lg mx-auto md:p-8 p-6 pt-5 md:mt-12 text-dark dark:text-light",
         className,
       )}
       {...props}
