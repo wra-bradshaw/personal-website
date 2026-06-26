8f0f5fe39c2c664438997c7952b57339fccb9fe4
will <will.bradshaw50@gmail.com>
2026-06-27 00:14:23 +1000

    feat: make container use light/dark vars

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ export function Container({ className, ...props }: ComponentProps<"article">) {
   return (
     <article
       className={cn(
-        "max-w-lg mx-auto md:p-8 px-6 pt-3 md:mt-12 text-gray-600 dark:text-gray-300",
+        "max-w-lg mx-auto md:p-8 px-6 pt-3 md:mt-12 text-dark dark:text-light",
         className,
       )}
       {...props}
