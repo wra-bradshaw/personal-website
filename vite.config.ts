134c767d2142db1187a13fad87e9a2559de02f35
will <will.bradshaw50@gmail.com>
2026-06-27 13:13:50 +1000

    feat: sitemap and prerendering

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig, normalizePath, type Plugin } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-import typst, { checkExecResult } from "@myriaddreamin/vite-plugin-typst";
-import { createHash } from "node:crypto";
-import { mkdirSync, writeFileSync } from "node:fs";
-import { dirname, join, relative } from "node:path";
-import { JSDOM } from "jsdom";
+import typst from "@myriaddreamin/vite-plugin-typst";
 
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
@@ -12,23 +8,35 @@ import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  server: {
-    allowedHosts: ["vz1vejrgtafq.shares.zrok.io"],
-  },
-  plugins: [
-    typst(),
-    devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-    tailwindcss(),
-    tanstackStart({
-      prerender: {
-        enabled: true,
-      },
-    }),
-    viteReact(),
-  ],
+const config = defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  const siteUrl = env.VITE_SITE_URL?.replace(/\/$/, "");
+
+  if (!siteUrl) {
+    throw new Error("Missing VITE_SITE_URL");
+  }
+
+  return {
+    resolve: { tsconfigPaths: true },
+    plugins: [
+      typst(),
+      devtools(),
+      nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+      tailwindcss(),
+      tanstackStart({
+        prerender: {
+          enabled: true,
+          crawlLinks: true,
+        },
+        sitemap: {
+          enabled: true,
+          host: siteUrl,
+        },
+      }),
+      viteReact(),
+    ],
+  };
 });
 
 export default config;
