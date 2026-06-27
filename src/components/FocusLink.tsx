b0fb6036f21ca75f3c061cc7a6040ab953d50c3a
will <will.bradshaw50@gmail.com>
2026-06-27 16:34:32 +1000

    feat: make focuslink polymorphic

--- src/components/FocusLink.tsx
+++ src/components/FocusLink.tsx
@@ -1,16 +1,24 @@
-import type { ComponentProps } from "react";
+import type { ComponentPropsWithoutRef, ElementType } from "react";
 import { Link } from "@tanstack/react-router";
 
 import { cn } from "#/lib/utils";
 import { FocusRing } from "./FocusRing";
 
-export function FocusLink({
+type FocusLinkProps<T extends ElementType = typeof Link> = {
+  as?: T;
+  className?: string;
+} & Omit<ComponentPropsWithoutRef<T>, "as" | "className">;
+
+export function FocusLink<T extends ElementType = typeof Link>({
+  as,
   className,
   ...props
-}: ComponentProps<typeof Link>) {
+}: FocusLinkProps<T>) {
+  const Component = as ?? Link;
+
   return (
     <FocusRing
-      as={Link}
+      as={Component}
       className={cn(
         "inset-text inset-underline transition-all hover:text-black hover:dark:text-white",
         className,
